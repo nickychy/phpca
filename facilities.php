ea48c434c8cffa451faadc70254023c6f95755a8
nickychy <96295935+nickychy@users.noreply.github.com>
2024-11-05 15:39:16 +0530

    first commit

---
+++ facilities.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>NS Hotel - HOME</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <?php require('inc/links.php') ?>
+</head>
+
+<body class="bg-light">
+
+
+    <?php require('inc/header.php') ?>
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+        <div class="h-line bg-dark">
+            <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias magni obcaecati
+                temporibus similique assumenda inventore rem soluta unde ad distinctio.</p>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 mb-5 px-4">
+                <div>
+                    <img src="images/facilities/WIFI.svg" width="40px">
+                    <h5>Wifi</h5>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptatum quae quas tenetur
+                        quis unde maiores vitae eveniet quia nulla a doloribus facere totam nam illum, quisquam dolor
+                        cumque id.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php require('inc/footer.php') ?>
+
+
+
+</body>
+
+</html>
