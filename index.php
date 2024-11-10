cc651d70be23a7d00b53a36abeaf96c57816d00f
nickychy <96295935+nickychy@users.noreply.github.com>
2024-11-10 18:34:02 +0530

    features_And_facilities

--- index.php
+++ index.php
@@ -33,28 +33,26 @@
     <div class="container-fluid px-lg-4 mt-4">
         <div class="swiper mySwiper">
             <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <img src="images/carousel/1.png" class="w-100 d-block" />
+                <?php
+                $res = selectAll('carousel');
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $path = CAROUSEL_IMG_PATH;
+                    echo <<<data
+                    
+                    <div class="swiper-slide">
+                    <img src="$path$row[image]" class="w-100 d-block" />
                 </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/2.png" class="w-100 d-block" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/3.png" class="w-100 d-block" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/4.png" class="w-100 d-block" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/5.png" class="w-100 d-block" />
-                </div>
-                <div class="swiper-slide">
-                    <img src="images/carousel/6.png" class="w-100 d-block" />
-                </div>
+                               
+                data;
+
+                }
+                ?>
+
 
             </div>
         </div>
     </div>
+
 
     <!-- Check Availability form  -->
 
